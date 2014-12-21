3f2ca024436c34a3332a9769e4693d812cd9ede3
Finalgamer14 <finalgamer15@gmail.com>
2014-12-21 13:05:56 +0100

    Inital Commit

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gImage = NULL;
+
+bool init()
+{
+    bool success = true;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        success = false;
+    }
+    else
+    {
+        gWindow = SDL_CreateWindow("SDL Event Driven Programing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL)
+        {
+            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+            success = false;
+        }
+        else
+        {
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    bool success = true;
+
+    gImage = SDL_LoadBMP("hello_world.bmp");
+    if(gImage == NULL)
+    {
+        printf("Unable to load image %s! SDL_Error: %s\n", "hello_world.bmp", SDL_GetError());
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    SDL_FreeSurface(gImage);
+    gImage = NULL;
+
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    SDL_Quit();
+}
+
+int main(int argc, char *argv[])
+{
+    if(!init())
+    {
+        printf("Failed to initialize!\n");
+    }
+    else
+    {
+        if(!loadMedia())
+        {
+            printf("Failed to load media!\n");
+        }
+        else
+        {
+            bool quit = false;
+            SDL_Event e;
+
+            while(!quit)
+            {
+                while(SDL_PollEvent(&e) != 0)
+                {
+                    if(e.type == SDL_QUIT)
+                    {
+                        quit = true;
+                    }
+                }
+
+                SDL_BlitSurface(gImage, NULL, gScreenSurface, NULL);
+                SDL_UpdateWindowSurface(gWindow);
+            }
+        }
+    }
+    close();
+
+    return 0;
+}
